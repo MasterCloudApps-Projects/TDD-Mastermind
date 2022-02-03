b9a93eeffe8eb3b01f6a321f5ce02ca7a2d13077
HarenderS <hs.dhanoya@alumnos.urjc.es>
2022-02-03 20:51:06 +0100

    Initial back proyect

---
+++ TFM-Mastermind-Back/src/main/java/com/tfm/mastermind/back/TfmMastermindBackApplication.java
@@ -0,0 +1,13 @@
+package com.tfm.mastermind.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfmMastermindBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfmMastermindBackApplication.class, args);
+	}
+
+}
---
+++ TFM-Mastermind-Back/src/test/java/com/tfm/mastermind/back/TfmMastermindBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfm.mastermind.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfmMastermindBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
