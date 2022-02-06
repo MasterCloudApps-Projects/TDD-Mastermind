5d3996b8805e46e5528500277a74d2faf5c8d6f2
HarenderS <hs.dhanoya@alumnos.urjc.es>
2022-02-06 22:04:06 +0100

    Add the test getRandomSecretCombinationTest and the code for it

--- TFM-Mastermind-Back/src/main/java/com/tfm/mastermind/back/models/SecretCombination.java
+++ TFM-Mastermind-Back/src/main/java/com/tfm/mastermind/back/models/SecretCombination.java
@@ -1,11 +1,22 @@
 package com.tfm.mastermind.back.models;
 
 import java.util.List;
+import java.util.Random;
 
 import com.tfm.mastermind.back.utils.Color;
 
 public class SecretCombination extends Combination{
 
+	public SecretCombination() {
+		Random random = new Random();
+		do {
+			int x = random.nextInt(Color.length());
+			if (!this.combination.contains(Color.get(x))) {
+				this.combination.add(Color.get(x));
+			}
+		} while (this.combination.size() < getMaxWidth());
+	}
+	
 	public List<Color> getColors(){
 		return this.combination;
 	}
--- TFM-Mastermind-Back/src/test/java/com/tfm/mastermind/back/models/SecretCombinationTest.java
+++ TFM-Mastermind-Back/src/test/java/com/tfm/mastermind/back/models/SecretCombinationTest.java
@@ -1,5 +1,6 @@
 package com.tfm.mastermind.back.models;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
@@ -10,5 +11,12 @@ public class SecretCombinationTest {
 	public void getSecretCombinationNotNullTest() {
 		SecretCombination secretCombination = new SecretCombination();
 		assertNotNull(secretCombination.getColors());
+	}
+	
+	@Test
+	public void getRandomSecretCombinationTest() {
+		SecretCombination secretCombination = new SecretCombination();
+		SecretCombination secretCombination2 = new SecretCombination();
+		assertNotEquals(secretCombination.getColors().toString(), secretCombination2.getColors().toString());
 	}
 }
