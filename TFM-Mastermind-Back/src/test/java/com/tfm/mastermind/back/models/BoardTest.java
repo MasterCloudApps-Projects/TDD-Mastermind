25f570cfa39016a536d542244edc57f5c5057c79
HarenderS <hs.dhanoya@alumnos.urjc.es>
2022-02-07 21:07:40 +0100

    Add the test getDiferentSecretCombinationTest

--- TFM-Mastermind-Back/src/test/java/com/tfm/mastermind/back/models/BoardTest.java
+++ TFM-Mastermind-Back/src/test/java/com/tfm/mastermind/back/models/BoardTest.java
@@ -1,6 +1,7 @@
 package com.tfm.mastermind.back.models;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +10,14 @@ public class BoardTest {
 	@Test
 	public void getBoardSecretCombinationNotNull() {
 		Board board = new Board();
-		assertNull(board.getSecretCombination());
+		assertNotNull(board.getSecretCombination());
+	}
+	
+	@Test
+	public void getDiferentSecretCombinationTest() {
+		Board board = new Board();
+		Board board2 = new Board();
+		assertNotEquals(board.getSecretCombination(), board2.getSecretCombination());
 	}
 	
 }
