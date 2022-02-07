bc7b70ebea6a0f50b6d8a9b5b54e0fdd48a321d1
HarenderS <hs.dhanoya@alumnos.urjc.es>
2022-02-07 20:52:42 +0100

    Add test getBoardSecretCombinationNull

---
+++ TFM-Mastermind-Back/src/main/java/com/tfm/mastermind/back/models/Board.java
@@ -0,0 +1,8 @@
+package com.tfm.mastermind.back.models;
+
+public class Board {
+
+	public SecretCombination getSecretCombination() {
+		return null;
+	}
+}
---
+++ TFM-Mastermind-Back/src/test/java/com/tfm/mastermind/back/models/BoardTest.java
@@ -0,0 +1,14 @@
+package com.tfm.mastermind.back.models;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.Test;
+
+public class BoardTest {
+
+	@Test
+	public void getBoardSecretCombinationNull() {
+		Board board = new Board();
+		assertNull(board.getSecretCombination());
+	}
+}
