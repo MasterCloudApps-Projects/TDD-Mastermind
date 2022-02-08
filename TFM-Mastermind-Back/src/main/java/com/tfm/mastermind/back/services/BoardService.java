62f31d20ff652af155504a332d8752210d6c386a
HarenderS <hs.dhanoya@alumnos.urjc.es>
2022-02-08 21:21:01 +0100

    Add the test getBoardNullTest

---
+++ TFM-Mastermind-Back/src/main/java/com/tfm/mastermind/back/services/BoardService.java
@@ -0,0 +1,10 @@
+package com.tfm.mastermind.back.services;
+
+import com.tfm.mastermind.back.models.Board;
+
+public class BoardService {
+
+	public Board getBoard() {
+		return null;
+	}
+}
---
+++ TFM-Mastermind-Back/src/test/java/com/tfm/mastermind/back/services/BoardServiceTest.java
@@ -0,0 +1,5 @@
+package com.tfm.mastermind.back.services;
+
+public class BoardServiceTest {
+
+}
