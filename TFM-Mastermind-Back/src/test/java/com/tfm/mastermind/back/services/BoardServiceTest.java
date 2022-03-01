2c6e5dbdd9d2ed589e79c4c1a95a4370dcf5ed45
HarenderS <hs.dhanoya@alumnos.urjc.es>
2022-03-01 21:33:05 +0100

    Add test addProposalCombinationToBoardTest

--- TFM-Mastermind-Back/src/test/java/com/tfm/mastermind/back/services/BoardServiceTest.java
+++ TFM-Mastermind-Back/src/test/java/com/tfm/mastermind/back/services/BoardServiceTest.java
@@ -1,8 +1,14 @@
 package com.tfm.mastermind.back.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
+
+import com.tfm.mastermind.back.models.ProposalCombination;
+import com.tfm.mastermind.back.utils.Color;
 
 public class BoardServiceTest {
 
@@ -22,5 +28,25 @@ public class BoardServiceTest {
 	public void getActualIntentFromBoardTest() {
 		BoardService boardService = new BoardService();
 		assertNotNull(boardService.getBoard().getActualIntent());
+	}
+	
+	@Test
+	public void addProposalCombinationToBoardTest() {
+		BoardService boardService = new BoardService();
+		ProposalCombination proposalCombination = new ProposalCombination();
+		proposalCombination.combination.add(Color.RED);
+		proposalCombination.combination.add(Color.BLUE);
+		proposalCombination.combination.add(Color.GREEN);
+		proposalCombination.combination.add(Color.ORANGE);
+		
+		int firstIntent = boardService.getBoard().getActualIntent();		
+		assertNull(boardService.getBoard().getProposalCombination(firstIntent));
+		assertNotEquals(proposalCombination, boardService.getBoard().getProposalCombination(firstIntent));
+		
+		boardService.getBoard().addProposal(proposalCombination);
+		
+		assertNotNull(boardService.getBoard().getProposalCombination(firstIntent));
+		assertEquals(proposalCombination, boardService.getBoard().getProposalCombination(firstIntent));
+		
 	}
 }
