4425653ac9079a2006955dc7fdcc0185d6febfb0
HarenderS <hs.dhanoya@alumnos.urjc.es>
2022-05-02 18:00:35 +0200

    Add addProposalCombinationRestTest and the code for it

--- TFM-Mastermind-Back/src/main/java/com/tfm/mastermind/back/controllers/BoardController.java
+++ TFM-Mastermind-Back/src/main/java/com/tfm/mastermind/back/controllers/BoardController.java
@@ -5,13 +5,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.tfm.mastermind.back.models.Board;
 import com.tfm.mastermind.back.models.ProposalCombination;
 import com.tfm.mastermind.back.services.BoardService;
 
-@Controller
+@RestController
 @RequestMapping("/api/board")
 public class BoardController {
 
@@ -23,7 +27,10 @@ public class BoardController {
 		return new ResponseEntity<>(this.boardService.getBoard(), HttpStatus.OK);
 	}
 	
-	public void addProposalCombination(ProposalCombination proposalCombination) {
+	@PutMapping("/")
+	@ResponseStatus(HttpStatus.OK)
+	public ProposalCombination addProposalCombination(@RequestBody ProposalCombination proposalCombination) {
 		this.boardService.addProposal(proposalCombination);
+		return this.boardService.getBoard().getProposalCombination(this.boardService.getBoard().getActualIntent()-1);
 	}
 }
--- TFM-Mastermind-Back/src/test/java/com/tfm/mastermind/back/controllers/rest/BoardControllerRestTest.java
+++ TFM-Mastermind-Back/src/test/java/com/tfm/mastermind/back/controllers/rest/BoardControllerRestTest.java
@@ -1,6 +1,7 @@
 package com.tfm.mastermind.back.controllers.rest;
 
 import static io.restassured.RestAssured.when;
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
@@ -10,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BoardControllerRestTest {
@@ -32,5 +34,25 @@ public class BoardControllerRestTest {
              .statusCode(200)
              .body(notNullValue())
              .body("secretCombination.combination.size()", equalTo(4));
+    }
+    
+    @Test
+	public void addProposalCombinationRestTest() throws Exception {
+    	
+    	given().
+        request()
+            .body("{ \"combination\" : [ \"BLUE\", \"YELLOW\", \"PURPLE\", \"GREEN\"] }")
+            .contentType(ContentType.JSON).
+        when()
+            .put("/api/board/")
+            
+        .then()
+            .assertThat()
+            .statusCode(200)
+            .body(notNullValue())
+	        .body("combination[0]", equalTo("BLUE"))//, YELLOW, PURPLE, GREEN]"));
+	        .body("combination[1]", equalTo("YELLOW"))//, YELLOW, PURPLE, GREEN]"));
+	        .body("combination[2]", equalTo("PURPLE"))//, YELLOW, PURPLE, GREEN]"));
+    		.body("combination[3]", equalTo("GREEN"));
     }
 }
