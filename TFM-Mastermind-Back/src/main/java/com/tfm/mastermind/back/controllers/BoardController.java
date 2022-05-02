9e30f46e7b38d84c67cfa799dec533860cf7ac44
HarenderS <hs.dhanoya@alumnos.urjc.es>
2022-05-02 14:25:49 +0200

    Add addProposalCombinationControllerTest and the code for it

--- TFM-Mastermind-Back/src/main/java/com/tfm/mastermind/back/controllers/BoardController.java
+++ TFM-Mastermind-Back/src/main/java/com/tfm/mastermind/back/controllers/BoardController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.tfm.mastermind.back.models.Board;
+import com.tfm.mastermind.back.models.ProposalCombination;
 import com.tfm.mastermind.back.services.BoardService;
+import com.tfm.mastermind.back.utils.Color;
 
 @Controller
 @RequestMapping("/api/board")
@@ -20,5 +22,16 @@ public class BoardController {
 	@GetMapping("/")
 	public ResponseEntity<Board> getBoard() {
 		return new ResponseEntity<>(this.boardService.getBoard(), HttpStatus.OK);
+	}
+	
+	public void addProposalCombination() {
+
+		ProposalCombination proposalCombination = new ProposalCombination();
+		proposalCombination.combination.add(Color.RED);
+		proposalCombination.combination.add(Color.BLUE);
+		proposalCombination.combination.add(Color.GREEN);
+		proposalCombination.combination.add(Color.ORANGE);
+		
+		this.boardService.addProposal(proposalCombination);
 	}
 }
--- TFM-Mastermind-Back/src/test/java/com/tfm/mastermind/back/controllers/unit/BoardControllerTest.java
+++ TFM-Mastermind-Back/src/test/java/com/tfm/mastermind/back/controllers/unit/BoardControllerTest.java
@@ -1,12 +1,14 @@
 package com.tfm.mastermind.back.controllers.unit;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.tfm.mastermind.back.controllers.BoardController;
+import com.tfm.mastermind.back.models.Board;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -18,5 +20,19 @@ public class BoardControllerTest {
 	@Test
 	public void getBoardNotNullFromControllerTest() {
 		assertNotNull(this.boardController.getBoard());
+	}
+	
+	@Test
+	public void addProposalCombinationControllerTest() {
+		
+		Board board = this.boardController.getBoard().getBody();
+		int firstIntent = board.getActualIntent();		
+		assertNull(board.getProposalCombination(firstIntent));
+		
+		this.boardController.addProposalCombination();
+		
+		assertNotNull(board.getProposalCombination(firstIntent));
+		
+		
 	}
 }
