45c88caba86b33855f693b2c222e990e9d8ae114
HarenderS <hs.dhanoya@alumnos.urjc.es>
2022-05-21 23:27:14 +0200

    Change the test getWrongWhiteValueTest() to getWhiteValueTest() and the code for it

--- TFM-Mastermind-Back/src/main/java/com/tfm/mastermind/back/models/Result.java
+++ TFM-Mastermind-Back/src/main/java/com/tfm/mastermind/back/models/Result.java
@@ -2,7 +2,13 @@ package com.tfm.mastermind.back.models;
 
 public class Result {
 
+	private int white;
+	
+	public Result() {
+		white = 0;
+	}
+	
 	public int getWhites() {
-		return Integer.MIN_VALUE;
+		return white;
 	}
 }
--- TFM-Mastermind-Back/src/test/java/com/tfm/mastermind/back/models/ResultTest.java
+++ TFM-Mastermind-Back/src/test/java/com/tfm/mastermind/back/models/ResultTest.java
@@ -7,8 +7,8 @@ import org.junit.jupiter.api.Test;
 public class ResultTest {
 
 	@Test
-	public void getWrongWhiteValueTest() {
+	public void getWhiteValueTest() {
 		Result result = new Result();
-		assertEquals(result.getWhites(), Integer.MIN_VALUE);		
+		assertEquals(result.getWhites(), 0);		
 	}
 }
