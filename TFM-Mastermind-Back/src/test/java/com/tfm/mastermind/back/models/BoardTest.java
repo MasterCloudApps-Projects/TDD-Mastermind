92357ccbbc61dd792144f8def3f79db1ab861100
HarenderS <hs.dhanoya@alumnos.urjc.es>
2022-05-21 21:57:15 +0200

    Add the test getNotEmptyResultBoardTest() and the code for it

--- TFM-Mastermind-Back/src/test/java/com/tfm/mastermind/back/models/BoardTest.java
+++ TFM-Mastermind-Back/src/test/java/com/tfm/mastermind/back/models/BoardTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import com.tfm.mastermind.back.utils.Color;
@@ -97,5 +99,14 @@ public class BoardTest {
 	public void getNotNullResultBoardTest() {
 		Board board = new Board();
 		assertNotNull(board.getResult());
+	}
+	
+	@Test
+	public void getNotEmptyResultBoardTest() {
+		Board board = new Board();
+		assertNotNull(board.getResult());
+		JSONObject result = new JSONObject(board.getResult());
+		assertTrue(result.has("black"));
+		assertTrue(result.has("white"));
 	}
 }
