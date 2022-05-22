c075d1980d1e9d402700606f073a30e5c3767113
HarenderS <hs.dhanoya@alumnos.urjc.es>
2022-05-22 03:31:15 +0200

    Add test addProposalAndCheckResultFuncionalityTest()

--- TFM-Mastermind-Back/src/test/java/com/tfm/mastermind/back/models/SecretCombinationTest.java
+++ TFM-Mastermind-Back/src/test/java/com/tfm/mastermind/back/models/SecretCombinationTest.java
@@ -1,5 +1,6 @@
 package com.tfm.mastermind.back.models;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -35,6 +36,25 @@ public class SecretCombinationTest {
 		assertNotNull(result);
 		assertNotNull(result.getBlack());
 		assertNotNull(result.getWhite());
+	}
+	
+	@Test
+	public void addProposalAndCheckResultFuncionalityTest() {
+		SecretCombination secretCombination = new SecretCombination();
+		ProposalCombination proposalCombination = new ProposalCombination();
+		proposalCombination.combination.add(Color.RED);
+		proposalCombination.combination.add(Color.BLUE);
+		proposalCombination.combination.add(Color.GREEN);
+		proposalCombination.combination.add(Color.ORANGE);
+		Result result = secretCombination.getResult(proposalCombination);
 		
+		assertNotNull(result);
+		assertNotEquals(result.getBlack(), 4);
+		
+		proposalCombination.combination = secretCombination.combination;
+		Result result2 = secretCombination.getResult(proposalCombination);
+		
+		assertNotEquals(result, result2);
+		assertEquals(result2.getBlack(), 4);
 	}
 }
