d3a458c3ae2676257bd59ba3a3d90bcc6ff78387
HarenderS <hs.dhanoya@alumnos.urjc.es>
2022-05-22 20:50:19 +0200

    Change the BoardService to use Board to get proposal list

--- TFM-Mastermind-Back/src/main/java/com/tfm/mastermind/back/services/BoardService.java
+++ TFM-Mastermind-Back/src/main/java/com/tfm/mastermind/back/services/BoardService.java
@@ -32,6 +32,6 @@ public class BoardService {
 	}
 	
 	public ProposalCombination[] getProposalCombinations() {
-		return new ProposalCombination[10];
+		return this.board.getProposalCombinations();
 	}
 }
